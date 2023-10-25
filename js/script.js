cc578a3b43e3c1dd9b20d714e30a10a9d6ab65f6
Andranik Torgomyan <andranik@nteams.com>
2023-10-25 19:09:23 +0400

    Fix bugs

--- js/script.js
+++ js/script.js
@@ -7,28 +7,20 @@ const {
 }
 
 AOS.init();
-const video = document.querySelector('video');
-video.muted = true;
-video.play()
 
-// const howItWorkingHeight = 2500;
 const howItWorkingSlider = $('.howItWorking-slider');
 const prevBtn = $('#prev-howItWorking');
 const nextBtn = $('#next-howItWorking');
 
-let imageLeft = 0;
-
 nextBtn.on('click', function (){
-    // imageLeft < checkWindowImage() ? imageLeft += checkWindow() : imageLeft = 0;
-    // $('#howItWorking img').css('transform', ` translateX(-${imageLeft}px)`);
-    howItWorkingSlider.scrollLeft(howItWorkingSlider.scrollLeft() + 300)
+    const scrollLeft = howItWorkingSlider.scrollLeft();
+    howItWorkingSlider.scrollLeft(scrollLeft + 300)
 })
-
 
 prevBtn.on('click', function () {
-    howItWorkingSlider.scrollLeft(howItWorkingSlider.scrollLeft() - 300)
+    const scrollLeft = howItWorkingSlider.scrollLeft();
+    howItWorkingSlider.scrollLeft(scrollLeft - 300);
 })
-
 
 function checkWindow(){
     const windowWidth = $(window).width();
@@ -39,9 +31,6 @@ function checkWindow(){
     }
 }
 
-
-//........ Slider
-
 $('#slider-one').slick({
     dots: true,
     infinite: true,
@@ -49,49 +38,11 @@ $('#slider-one').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
-    // customPaging: function (slider, i) {
-    //     console.log((i + 1) + '/' + slider.slideCount, '2222')
-    //
-    //     return  (i + 1) + '/' + slider.slideCount;
-    //
-    // },
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-        }
-
-    ]
+    autoplaySpeed: 2000
 }).on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-    //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
-    if(!slick.$dots){
-        return;
-    }
-
     const i = (currentSlide ? currentSlide : 0) + 1;
-
     $('.count.slider-one span').text(i + '/' + (slick.$dots[0].children.length));
 })
-
 
 $('#slider-two').slick({
     dots: true,
@@ -100,40 +51,8 @@ $('#slider-two').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
-    // customPaging: function (slider, i) {
-    //     console.log((i + 1) + '/' + slider.slideCount, '2222')
-    //
-    //     return  (i + 1) + '/' + slider.slideCount;
-    //
-    // },
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-            }
-        },
-        {
-            breakpoint: 600,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-        },
-        {
-            breakpoint: 480,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-            }
-        }
-
-    ]
+    autoplaySpeed: 2000
 }).on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
-    //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
     if(!slick.$dots){
         return;
     }
@@ -147,7 +66,7 @@ $(window).on('resize', function (){
     const containerWidth = $('.container').width();
     const result = containerWidth + ((windowWidth - containerWidth) / 2);
 
-    $('.howItWorking-slider').css('width', `${result}px`)
+    $('.howItWorking-slider').css('width', `${result}px`);
 }).resize()
 
 
